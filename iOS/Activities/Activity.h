02f32811ad2fe2630baa3a17b81b12e895eed4b4
Owen Campbell-Moore <owencmoore@gmail.com>
2013-07-28 00:20:27 -0700

    RestKit now receives activities from the server and turns them into instances of the Activity class

---
+++ iOS/Activities/Activity.h
@@ -0,0 +1,13 @@
+//
+//  Activity.h
+//  Activities
+//
+//  Created by Owen Campbell-Moore on 27/07/2013.
+//  Copyright (c) 2013 A&O. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Activity : NSObject
+
+@end
