b5ff9bf12ce358bf01e665f7ca919d92da6f83fe
Ally Gale <allygale@gmail.com>
2013-07-27 20:56:35 -0700

    Made activities json serializable

--- tapalong/tapalong_app/urls.py
+++ tapalong/tapalong_app/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from tapalong_app import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<user_id>\d+)/$', views.get_events_list, name='get_user_events_list'),
+    url(r'^(?P<user_id>\d+)/$', views.get_activities_list, name='get_user_activities_list'),
 )
--- tapalong/tapalong_app/views.py
+++ tapalong/tapalong_app/views.py
@@ -4,12 +4,17 @@ import datetime
 
 from tapalong_app.models import User, Activity
 
+# Serializes a single activity, passing along the following:
+# Title, start time, description, location, max attendees
+def serialize_activity(activity):
+	return {"title": activity.title, "start_time": str(activity.start_time), "description": activity.description, "location": activity.location, "max_attendees": activity.max_attendees}
+
 # Returns all events for the given user. Events are
 # returns in order of creation; youngest to oldest.
-def get_events_list(request, user_id):
-	user_events_list = Activity.objects.filter(attendees=user_id).order_by('-pub_date')
-	output = ', '.join([a.title for a in user_events_list])
-
-	return HttpResponse(output)
+def get_activities_list(request, user_id):
+	user_activities_list = Activity.objects.filter(attendees=user_id).order_by('-pub_date')
+	serialized_activities = map(serialize_activity, user_activities_list)
+	json_output = json.dumps(serialized_activities)
+	return HttpResponse(json_output, mimetype='application/json')
 
 
