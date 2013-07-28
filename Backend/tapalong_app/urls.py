ab41d7f6bb9af7a8ae7fcad0195fb7bf108349fc
Ally Gale <allygale@gmail.com>
2013-07-28 09:13:31 -0700

    fixing merge conflicts

--- Backend/tapalong_app/urls.py
+++ Backend/tapalong_app/urls.py
@@ -4,5 +4,5 @@ from tapalong_app import views
 
 urlpatterns = patterns('',
     url(r'^(?P<user_id>\d+)/$', views.get_activities_list, name='get_user_activities_list'),
-    # url(r'^(?P<user_id>\d+)/create_activity/$', views.create_activity, name='create_activity'),
+    url(r'^(?P<user_id>\d+)/create_activity/$', views.create_activity, name='create_activity'),
 )
