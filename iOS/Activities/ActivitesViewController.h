204894270cf79d7bb22923b590a8548e94539459
Owen Campbell-Moore <owencmoore@gmail.com>
2013-07-27 20:34:46 -0700

    iOS first commit

---
+++ iOS/Activities/ActivitesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActivitesViewController.h
+//  Activities
+//
+//  Created by Owen Campbell-Moore on 27/07/2013.
+//  Copyright (c) 2013 A&O. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivitesViewController : UITableViewController
+@property NSMutableArray *activities;
+@end
---
+++ iOS/Activities/ActivitesViewController.m
@@ -0,0 +1,94 @@
+//
+//  ActivitesViewController.m
+//  Activities
+//
+//  Created by Owen Campbell-Moore on 27/07/2013.
+//  Copyright (c) 2013 A&O. All rights reserved.
+//
+
+#import "ActivitesViewController.h"
+#import "ActivityTableCell.h"
+
+@interface ActivitesViewController ()
+
+@end
+
+@implementation ActivitesViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.activities = [[NSMutableArray alloc] init];
+    [self.activities addObject:@"Owen Campbell-Moore is blah"];
+    [self.activities addObject:@"Ally Gale is blah"];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.activities count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell Identifier";
+    ActivityTableCell *cell = (ActivityTableCell *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil)
+    {
+        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"ActivityTableCell" owner:self options:nil];
+        cell = [nib objectAtIndex:0];
+    }
+    
+    cell.activityLabel.text = [self.activities objectAtIndex:indexPath.row];
+    //    cell.thumbnailImageView.image = [UIImage imageNamed:[thumbnails objectAtIndex:indexPath.row]];
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 80;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
