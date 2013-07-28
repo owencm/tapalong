9e8a476bcb9acf591244b957bee5548468583f3a
Ally Gale <allygale@gmail.com>
2013-07-27 21:03:46 -0700

    removed tapalong folder

--- tapalong/tapalong_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-from tapalong_app import views
-
-urlpatterns = patterns('',
-    url(r'^(?P<user_id>\d+)/$', views.get_activities_list, name='get_user_activities_list'),
-)
