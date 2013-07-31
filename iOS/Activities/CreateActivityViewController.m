7c1f34d1408382d7d397488791a03026eaae487a
Owen Campbell-Moore <owencmoore@gmail.com>
2013-07-31 02:05:49 -0700

    CreateActivity now submits a request correctly, to be refactored into GlobalNetwork later

--- iOS/Activities/CreateActivityViewController.m
+++ iOS/Activities/CreateActivityViewController.m
@@ -6,8 +6,10 @@
 //  Copyright (c) 2013 A&O. All rights reserved.
 //
 
+#import <RestKit/RestKit.h>
 #import "CreateActivityViewController.h"
 #import "GlobalColors.h"
+#import "Activity.h"
 
 @interface CreateActivityViewController ()
 
@@ -19,7 +21,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        [[self view] setBackgroundColor:[[GlobalColors sharedGlobal] backgroundGrey] ];
+        [[self view] setBackgroundColor:[[GlobalColors sharedGlobal] backgroundGrey]];
     }
     return self;
 }
@@ -27,7 +29,41 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    Activity *activity = [[Activity alloc] init];
+    activity.title = @"title";
+    activity.description = @"description";
+    activity.location = @"location";
+    activity.max_attendees = @"maxatendees";
+    activity.start_time = @"time";
+    
+    [self requestCreateActivity:activity];
+}
+
+- (void)requestCreateActivity:(Activity *)activity
+{
+    RKObjectManager *manager = [RKObjectManager managerWithBaseURL:[NSURL URLWithString:@"http://127.0.0.1:8000"]];
+    
+    // Define the object mapping
+    RKObjectMapping *activityMapping = [RKObjectMapping requestMapping];
+    // TODO: These are the symmetric so it doesn't matter, but using the same for requests and responses requires the use of [mapping inverseMapping]
+    [activityMapping addAttributeMappingsFromDictionary:@{
+         @"title": @"title",
+         @"start_time": @"start_time",
+         @"location": @"location",
+         @"max_attendees": @"max_attendees",
+         @"description": @"description"
+     }];
+    
+    RKRequestDescriptor *requestDescriptor = [RKRequestDescriptor requestDescriptorWithMapping:activityMapping objectClass:[Activity class] rootKeyPath:nil method:RKRequestMethodPOST];
+    
+    [manager addRequestDescriptor: requestDescriptor];
+    
+    [manager postObject:activity path:@"/activities/1/" parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
+        NSLog(@"\n\nSuccessful Post!\n");
+    } failure:^(RKObjectRequestOperation *operation, NSError *error) {
+        NSLog(@"\n\nFail|n");
+    }];
 }
 
 - (void)didReceiveMemoryWarning
