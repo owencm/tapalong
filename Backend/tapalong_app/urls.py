d48e1ea963a7b3b5b156dcc185864d3460773b28
Ally Gale <allygale@gmail.com>
2014-10-12 16:05:56 -0700

    Reading user_id from headers instead of URL
    eom.

--- Backend/tapalong_app/urls.py
+++ Backend/tapalong_app/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from tapalong_app import views
 
 urlpatterns = patterns('',
-    url(r'^v1/activities/visible_to_user/(?P<user_id>\d+)/$', views.activities_list, name='user_activities_list'),
-    url(r'^v1/activities/(?P<activity_id>\d+)/attend/(?P<user_id>\d+)/$', views.attending, name='attending'),
-    url(r'^v1/activities/(?P<activity_id>\d+)/(?P<user_id>\d+)/$', views.activity, name='activity'), 
+    url(r'^v1/activities/visible_to_user/$', views.activities_list, name='user_activities_list'),
+    url(r'^v1/activities/(?P<activity_id>\d+)/attend/$', views.attending, name='attending'),
+    url(r'^v1/activities/(?P<activity_id>\d+)/$', views.activity, name='activity'), 
     url(r'^v1/login/$', views.login_user, name='login_user'),
 )
