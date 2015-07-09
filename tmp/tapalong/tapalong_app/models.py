ba646b65e3170758b6f6942a5d619f035748a888
Owen Campbell-Moore <owencmoore@gmail.com>
2015-07-08 21:01:42 -0700

    Renaming Backend temporarily

--- tmp/tapalong/tapalong_app/models.py
+++ tmp/tapalong/tapalong_app/models.py
@@ -3,7 +3,7 @@ from django.utils.timezone import utc
 import datetime
 
 class User(models.Model):
-	name = models.CharField(max_length = 20, blank=False)
+	name = models.CharField(max_length = 30, blank=False)
 	password = models.CharField(max_length = 30)
 	fb_id = models.IntegerField(blank=False)
 	friends = models.TextField(default='')
@@ -22,8 +22,8 @@ class Activity(models.Model):
 	creator = models.ForeignKey(User, related_name='event_set', blank=False)
 	pub_date = models.DateTimeField(auto_now_add=True)
 	start_time = models.DateTimeField(blank=False)
-	title = models.CharField(max_length=30, blank=False)
-	description = models.CharField(max_length=300, default='')
+	title = models.CharField(max_length=60, blank=False)
+	description = models.CharField(max_length=1024, default='')
 	attendees = models.ManyToManyField(User, related_name='attending_set')
 	location = models.CharField(max_length=60)
 	# 0 will represent no max here.
