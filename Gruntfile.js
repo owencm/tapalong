70abd55bb097503f3bd28877dc8519bfdfd80e47
Owen Campbell-Moore <owencmoore@gmail.com>
2015-10-15 18:47:44 -0700

    Refactored project layout

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: ['tmp/'],
+    babel: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'client/js/',
+          src: ['*.js'],
+          dest: 'tmp/'
+        },{
+          expand: true,
+          cwd: 'client/js/sw/',
+          src: ['*.js'],
+          dest: 'tmp/'
+        }]
+      }
+    },
+    browserify: {
+      options: {
+        browserifyOptions: {
+          debug: true
+        }
+      },
+      dist: {
+        files: {
+          'tapalong/tapalong_app/static/main.js': ['tmp/main.js'],
+          'tapalong/tapalong_app/static/sw.js': ['tmp/sw.js']
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          // includes files within path and its sub-directories
+          {expand: true, cwd: 'client/', src: ['*.html'], dest: 'tapalong/tapalong_app/static/'},
+          {expand: true, cwd: 'client/css/', src: ['*.css'], dest: 'tapalong/tapalong_app/static/'},
+          {expand: true, cwd: 'client/images/', src: ['**'], dest: 'tapalong/tapalong_app/static/images/'},
+        ],
+      },
+    },
+    watch: {
+      scripts: {
+        files: ['client/**'],
+        tasks: ['default'],
+        options: {
+          spawn: false,
+          atBegin: true
+        },
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task(s).
+  grunt.registerTask('default', ['babel', 'browserify', 'copy', 'clean']);
+
+};
--- backend/tapalong/tapalong_app/static/.module-cache/7f88c2f2bc273da264b25d7c98f1680288bf029c.js
+++
@@ -1,7 +0,0 @@
-// To account for the clipping bug filed against Doug in Chrome
-if (this.props.circular) {
-  overlayStyle.borderRadius = '50%';
-  overlayStyle.overflow = 'hidden';
-  imgStyle.borderRadius = '50%';
-  imgStyle.overflow = 'hidden';
-}
