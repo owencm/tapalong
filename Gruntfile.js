046714b991d43b4f33692f6f79b2c374b274ec19
Owen Campbell-Moore <owencmoore@gmail.com>
2015-10-21 23:49:12 -0700

    Added an icon and tidied up static resources

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function(grunt) {
       main: {
         files: [
           // includes files within path and its sub-directories
-          {expand: true, cwd: 'client/', src: ['*.html', 'manifest.json'], dest: 'tapalong/tapalong_app/static/'},
+          {expand: true, cwd: 'client/', src: ['*.html', '*.json'], dest: 'tapalong/tapalong_app/static/'},
           {expand: true, cwd: 'client/css/', src: ['*.css'], dest: 'tapalong/tapalong_app/static/'},
           {expand: true, cwd: 'client/images/', src: ['**'], dest: 'tapalong/tapalong_app/static/images/'},
         ],
