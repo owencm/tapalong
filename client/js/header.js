291333e2c13b7a7c4b7da16f33b17e7a2d28cd47
Owen Campbell-Moore <owencmoore@gmail.com>
2015-11-11 22:04:12 -0800

    Header is now a pure function

--- client/js/header.js
+++ client/js/header.js
@@ -2,54 +2,52 @@
 var React = require('react');
 var m = require('./m.js');
 
-module.exports = React.createClass({
-  render: function () {
-    var headerStyle = {
-      backgroundColor: '#00BCD4',
-      boxShadow: '0 1px 6px rgba(0,0,0,.2)',
-      position: 'fixed',
-      width: '100%',
-      zIndex: '1',
-      boxSizing: 'border-box'
-    }
-
-    // Note we set paddingLeft explicitely as if we don't, React creates a strange bug
-    // where it keeps the paddingLeft: 0 set later even if !shouldShowBackButton
-    var titleStyle = {
-      fontSize: '18px',
-      color: 'white',
-      fontWeight: 'bold',
-      padding: '19px',
-      paddingLeft: '19px',
-      float: 'left'
-    }
-
-    if (this.props.shouldShowBackButton) {
-      console.log('set titlestyle')
-      titleStyle = Object.assign({}, titleStyle, {paddingLeft: '0'});
-    }
-
-    var backButtonStyle = {
-    	width: '20px',
-    	padding: '18px',
-      float: 'left'
-    }
-
-    return (
-      <header style={headerStyle}>
-        {
-          this.props.shouldShowBackButton ?
-            <img
-              src='images/back-icon.svg'
-              style={backButtonStyle}
-              onClick={this.props.onBackButtonClick}
-            /> :
-            null
-        }
-        <h1 style={titleStyle}>
-          {this.props.title}
-        </h1>
-      </header>
-    );
+// A pure function, ala "Stateless function components" in https://facebook.github.io/react/blog/2015/09/10/react-v0.14-rc1.html
+module.exports = (props) => {
+  var headerStyle = {
+    backgroundColor: '#00BCD4',
+    boxShadow: '0 1px 6px rgba(0,0,0,.2)',
+    position: 'fixed',
+    width: '100%',
+    zIndex: '1',
+    boxSizing: 'border-box'
   }
-})
+
+  // Note we set paddingLeft explicitely as if we don't, React creates a strange bug
+  // where it keeps the paddingLeft: 0 set later even if !shouldShowBackButton
+  var titleStyle = {
+    fontSize: '18px',
+    color: 'white',
+    fontWeight: 'bold',
+    padding: '19px',
+    paddingLeft: '19px',
+    float: 'left'
+  }
+
+  if (props.shouldShowBackButton) {
+    titleStyle = Object.assign({}, titleStyle, {paddingLeft: '0'});
+  }
+
+  var backButtonStyle = {
+  	width: '20px',
+  	padding: '18px',
+    float: 'left'
+  }
+
+  return (
+    <header style={headerStyle}>
+      {
+        props.shouldShowBackButton ?
+          <img
+            src='images/back-icon.svg'
+            style={backButtonStyle}
+            onClick={props.onBackButtonClick}
+          /> :
+          null
+      }
+      <h1 style={titleStyle}>
+        {props.title}
+      </h1>
+    </header>
+  );
+};
