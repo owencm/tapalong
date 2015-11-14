3efbd0ced1a4816a3081d97ab594d5e8bde76af9
Owen Campbell-Moore <owencmoore@gmail.com>
2015-11-13 22:44:29 -0800

    Fixed activity orderings

--- client/js/reducers.js
+++ client/js/reducers.js
@@ -9,31 +9,31 @@ import {
 
 // TODO: Refactor to somewhere else
 let validateNewActivity = function (activity) {
-    // TODO: Validate values of the properties
-    // TODO: Validate client generated ones separately to server given ones
-    let properties = ['max_attendees', 'description', 'start_time', 'title', 'location'];
-    let hasProperties = properties.reduce(function(previous, property) {
-      return (previous && activity.hasOwnProperty(property));
-    }, true);
-    if (!hasProperties) {
-      return {isValid: false, reason: 'some properties were missing'};
+  // TODO: Validate values of the properties
+  // TODO: Validate client generated ones separately to server given ones
+  let properties = ['max_attendees', 'description', 'start_time', 'title', 'location'];
+  let hasProperties = properties.reduce(function(previous, property) {
+    return (previous && activity.hasOwnProperty(property));
+  }, true);
+  if (!hasProperties) {
+    return {isValid: false, reason: 'some properties were missing'};
+  }
+  if (activity.title == '') {
+    return {isValid: false, reason: 'missing title'};
+  }
+  if (!activity.start_time || !(activity.start_time instanceof Date)) {
+    return {isValid: false, reason: 'start_time wasnt a date object or was missing'};
+  }
+  if (activity.start_time && activity.start_time instanceof Date) {
+    // Allow users to see and edit events up to 2 hours in the past
+    let now = new Date;
+    now = now.add(-2).hours();
+    if (activity.start_time < now) {
+      return {isValid: false, reason: 'date (' + activity.start_time.toString() + ') was in the past'};
     }
-    if (activity.title == '') {
-      return {isValid: false, reason: 'missing title'};
-    }
-    if (!activity.start_time || !(activity.start_time instanceof Date)) {
-      return {isValid: false, reason: 'start_time wasnt a date object or was missing'};
-    }
-    if (activity.start_time && activity.start_time instanceof Date) {
-      // Allow users to see and edit events up to 2 hours in the past
-      let now = new Date;
-      now = now.add(-2).hours();
-      if (activity.start_time < now) {
-        return {isValid: false, reason: 'date (' + activity.start_time.toString() + ') was in the past'};
-      }
-    }
-    return {isValid: true};
-  };
+  }
+  return {isValid: true};
+};
 
 export function screens(state = {
   screen: SCREEN.uninitialized
@@ -80,7 +80,7 @@ export function activities(state = {
         {
           activities: [...state.activities,
             Object.assign({}, action.activity, {id: state.maxActivityId})
-          ],
+          ].sort((a, b) => { return a.start_time < b.start_time ? -1 : 1 }),
           maxActivityId: state.maxActivityId + 1
         }
       );
