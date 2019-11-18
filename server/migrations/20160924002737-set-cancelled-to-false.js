7cbca04d8062ab617c0436a59977eec5cca0fb2b
Owen Campbell-Moore <owencmoore@gmail.com>
2019-11-17 16:47:54 -0800

    Made initial Expo shell to migrate to

--- server/migrations/20160924002737-set-cancelled-to-false.js
+++ server/migrations/20160924002737-set-cancelled-to-false.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    queryInterface.sequelize.query('UPDATE `SQPlans` AS `SQPlan` SET `SQPlan`.`cancelled` = FALSE WHERE `SQPlan`.`cancelled` IS NULL;')
+    return queryInterface.sequelize.query('UPDATE `SQPlans` AS `SQPlan` SET `SQPlan`.`cancelled` = FALSE WHERE `SQPlan`.`cancelled` IS NULL;')
   },
 
   down: function (queryInterface, Sequelize) {
-    queryInterface.sequelize.query('UPDATE `SQPlans` AS `SQPlan` SET `SQPlan`.`cancelled` = NULL WHERE `SQPlan`.`cancelled` = FALSE;')
+    return queryInterface.sequelize.query('UPDATE `SQPlans` AS `SQPlan` SET `SQPlan`.`cancelled` = NULL WHERE `SQPlan`.`cancelled` = FALSE;')
   }
 };
